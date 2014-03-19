7cdc1ba77ed3532e2001640a889e12570d07016c
emilyhmoore <emily.moore@wustl.edu>
2014-03-19 09:44:49 -0500

    Start of a Package
    Start of a package. Has regcombo-class and the comboreg function, which
    will become fitBMA

---
+++ BMApack/R/regcombo-class.R
@@ -0,0 +1,73 @@
+#' A regression combination class
+#' 
+#' Object of class \code{regcombo} as created by the \code{comboreg} functions
+#'
+#' 
+#' An object of the class `regcombo' has the following slots:
+#' \itemize{
+#' \item \code{combo.coef} A list of coefficients for each possible regression
+#' \item \code{combo.fit} A vector of R-Squared values
+#' \item \code{x} A matrix input of covariates
+#' \item \code{y} The dependent variable input  
+#' }
+#'
+#' @author Emily Moore: \email{emily.moore@@wustl.edu}
+#' @export
+setClass(Class="regcombo",
+         representation = representation(
+           combo.coef = "list",
+           combo.fit="numeric",
+           x="matrix",
+           y="numeric"
+         ),
+         prototype = prototype(
+           combo.coef=list(),
+           combo.fit=numeric(),
+           x = matrix(),
+           y = numeric()
+         )
+)
+
+#' @export
+setMethod("initialize", "regcombo",
+          function(.Object, ...){
+            value=callNextMethod()
+            return(value)
+          }
+) 
+
+#' @export
+setMethod("show", "regcombo",
+          function(object){
+            cat("*****Regression Fits*****", "\n")
+            print(object@combo.fit)
+            cat("*****Regression Coefficients*****", "\n")
+            print(object@combo.coef)
+          }
+)
+
+#' @export
+
+setMethod(f="plot", signature="regcombo",
+          definition=function(x){
+            plot.default(x=(1:length(x@combo.fit)),y=x@combo.fit, 
+                 xlab="Regression Number" , ylab="R-Squared", 
+                 type="o", col="purple", main="R-Squared Over Models Run", pch="*")
+                 abline(h=x@avg.fit, col="red")
+          }
+          )
+
+#' @export 
+setGeneric("getcombo.fit",
+           function(object="regcombo")  {
+             standardGeneric("getcombo.fit")
+           }
+)
+#' @export
+setMethod("getcombo.fit", "regcombo",
+          function(object){
+            return(object@combo.fit)
+          }
+)
+
+
