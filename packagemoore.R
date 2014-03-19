fb9d1f6558dd0b9134da851ec70bd8734ba89fb2
emilyhmoore <emily.moore@wustl.edu>
2014-03-19 09:38:39 -0500

    Initial Dev File

---
+++ packagemoore.R
@@ -0,0 +1,19 @@
+library(devtools)
+library(roxygen2)
+find_rtools() ##For using devtools with windows. 
+setwd("~/GitHub/ASPMidterm")
+create(path="./BMApack", check=FALSE)
+
+current.code <- as.package("BMAPack")
+load_all(current.code)
+document(current.code)
+
+check(current.code)
+
+install(pkg=current.code, local=TRUE)
+
+##Example data
+
+help(comboreg)
+
+
